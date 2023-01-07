a3a249035dc1c75fe1dfa553a34a1dbeabe723c0
waihura <waihura.m@gmail.com>
2023-01-07 21:59:20 +0300

    Provider publishing fix

--- src/MpesaServiceProvider.php
+++ src/MpesaServiceProvider.php
@@ -3,6 +3,8 @@
 namespace MainaDavid\MultiShortcodeMpesa;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 
 class MpesaServiceProvider extends ServiceProvider
 {
@@ -23,5 +25,24 @@ class MpesaServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../database/migrations/create_short_codes_table.stub' => $this->getMigrationFileName('create_short_codes_table.php'),
         ], 'migrations');
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @return string
+     */
+    protected function getMigrationFileName($migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make($this->app->databasePath() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem, $migrationFileName) {
+                return $filesystem->glob($path . '*_' . $migrationFileName);
+            })
+            ->push($this->app->databasePath() . "/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 }
